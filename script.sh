9fc08165d3741adff1d3652494811a60189b5108
samt07 <miscapps111@gmail.com>
2016-05-24 22:51:42 -0400

    script.sh

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WORKDIR= pwd
+
+if [ ! -d $WORKDIR/ve ]; then
+    python3 -m venv ve
+    echo "Virtualenv created."
+    source ve/bin/activate
+    pip install django
+    pip install gunicorn
+fi
+exit
+
+#if [ ! -f "$WORKDIR/ve/updated" -o $WORKDIR/requirements.pip -nt $WORKDIR/ve/updated ]; then
+ #   pip install django $WORKDIR/ve
+  #  touch $WORKDIR/ve/updated
+   # echo "Requirements installed."
+#fi
